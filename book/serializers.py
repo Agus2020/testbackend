d40f6c6836ca93c3977a708e0efed4bab4c595d3
agus2020 <aagustin067@gmail.com>
2021-09-17 00:52:26 -0300

    ErrorApi

---
+++ book/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import *
+
+class LibrarySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Library
+        fields = ['name']
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'
+
+class LeadsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Leads
+        fields = '__all__'         
--- book/views.py
+++ book/views.py
@@ -1,31 +1,41 @@
 from django.views.generic import ListView
+from rest_framework import viewsets
+from .models import Library,Author,Book
+from .serializers import LibrarySerializer,AuthorSerializer,BookSerializer,LeadsSerializer
+from .models import Book, Author, Library,Leads
 
-from .models import Book, Author, Library
 
-
-class BookListView(ListView):
+class BookListView(ListView,viewsets.ModelViewSet):
     paginate_by = 100
     model = Book
     context_object_name = 'books'
-
+    serializers_class = BookSerializer
+    queryset = Book.objects.all()
     def get_queryset(self):
         qs = super(BookListView, self).get_queryset()
         qs.order_by('title')
         return qs
 
 
-class AuthorListView(ListView):
+class AuthorListView(ListView,viewsets.ModelViewSet):
     paginate_by = 100
     model = Author
     context_object_name = 'authors'
+    serializer_class = AuthorSerializer
+    queryset = Author.objects.all()
 
-
-class LibraryListView(ListView):
+class LibraryListView(ListView,viewsets.ModelViewSet):
     paginate_by = 10
     model = Library
     context_object_name = 'libraries'
+    serializer_class = LibrarySerializer
+    queryset = Library.objects.all()
 
+class LeadsListView(ListView,viewsets.ModelViewSet):
+    model = Leads
+    serializer_class = LeadsSerializer
+    queryset = Library.objects.all()
 
-book_list_view = BookListView.as_view()
-author_list_view = AuthorListView.as_view()
-library_list_view = LibraryListView.as_view()
+#book_list_view = BookListView.as_view()
+#author_list_view = AuthorListView.as_view()
+#library_list_view = LibraryListView.as_view()
