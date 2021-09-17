8a12ef21016ebf100c2d77c982e7971b58ba711d
agus2020 <aagustin067@gmail.com>
2021-09-16 22:53:09 -0300

    fase1

---
+++ book/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+class Library(models.Model):
+
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey('book.Author', on_delete=models.CASCADE)
+    libraries = models.ManyToManyField('book.Library')
+    def __str__(self):
+        return self.title
+
+
+class Author(models.Model):
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.first_name
+
+class Leads(models.Model):
+    Email = models.EmailField(max_length=100)
+    Fullname = models.CharField(max_length=40)
+    Phone = models.DecimalField(max_digits=10,decimal_places=1)
+    Library = models.ForeignKey(Library, on_delete=models.CASCADE)
