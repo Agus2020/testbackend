087061d32d815e21f25bc6c79b4e7e4f62df7dfb
Agus2020 <aagustin067@gmail.com>
2021-09-25 19:59:34 -0300

    Agregando acciones a las views

--- book/views.py
+++ book/views.py
@@ -6,10 +6,11 @@ from .models import Library,Author,Book
 from .serializers import *
 from .models import Book, Author, Library,Leads
 from rest_framework import viewsets, mixins
+from django.http import Http404
+from rest_framework.response import Response
+from rest_framework import status
 
-
-class LibraryListView(
-viewsets.GenericViewSet,
+class LibraryListView(viewsets.GenericViewSet,
 mixins.ListModelMixin,
 mixins.CreateModelMixin,
 mixins.DestroyModelMixin,
@@ -20,17 +21,31 @@ mixins.UpdateModelMixin):
     serializer_class = LibrarySerializer
     queryset = Library.objects.all()
 
+    def get_object(self, pk):
+        try:
+            return Library.objects.get(pk=pk)
+        except Library.DoesNotExist:
+            raise Http404
 
-class LibraryListView_filter(
-viewsets.GenericViewSet,
-mixins.ListModelMixin):
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = LibrarySerializer(snippet)
+        return Response(serializer.data)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+class LibraryListView_filter(viewsets.GenericViewSet,
+mixins.ListModelMixin
+):
     paginate_by = 10
     model = Library
     context_object_name = 'libraries_filter'
-    serializer_class = LibrarySerializer_filter
+    serializer_class = LibrarySerializer
     queryset = Library.objects.all()
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['id']
 
 
 class BookListView(
@@ -44,11 +59,26 @@ mixins.UpdateModelMixin):
     serializer_class = BookSerializer
     queryset = Book.objects.all()
 
+
+    def get_object(self, pk):
+        try:
+            return Book.objects.get(pk=pk)
+        except Book.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = BookSerializer(snippet)
+        return Response(serializer.data)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
 class BookListView_search(
 viewsets.GenericViewSet,
 mixins.ListModelMixin):
     paginate_by = 100
-    model = Book
+    model = Book_search
     context_object_name = 'books_search'
     serializer_class = BookSerializer_search
     queryset = Book.objects.all()
@@ -66,7 +96,19 @@ mixins.UpdateModelMixin):
     serializer_class = AuthorSerializer
     queryset = Author.objects.all()
 
+    def get_object(self, pk):
+        try:
+            return Author.objects.get(pk=pk)
+        except Author.DoesNotExist:
+            raise Http404
 
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = AuthorSerializer(snippet)
+        return Response(serializer.data)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
 class LeadsListView(
 viewsets.GenericViewSet,
